05ab44fcd86c75d147863a7d7ec47dad00771c40
Ahmed Khalid <ahmedkhalid958666@gmail.com>
2024-11-05 14:03:33 +0200

    2.2 e,f Finished

--- Telecom_Team_9.sql
+++ Telecom_Team_9.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE Telecom_Team_9;
 
+GO;
 CREATE VIEW AllShops AS
 SELECT *
 FROM Shop S LEFT JOIN Physical_Shop PS ON S.shopID = PS.shopID LEFT JOIN E_shop ON S.shopID = ES.shopID;
+
+GO;
+CREATE VIEW allResolvedTickets AS
+SELECT *
+FROM Technical_Support_Ticket
+WHERE status = 'resolved';
