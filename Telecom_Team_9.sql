9290c43d8c5a5c38eef0e4a5436a416722f2f47a
Ahmed Khalid <ahmedkhalid958666@gmail.com>
2024-11-05 21:03:04 +0200

    2.3 f (Changed Join)

--- Telecom_Team_9.sql
+++ Telecom_Team_9.sql
@@ -23,8 +23,8 @@ AS
 	BEGIN
 		SELECT @TotalTransactions = COUNT(P.PaymentID) , @TotalPoints = SUM(ISNULL(PG.pointsAmount,0))
 		FROM Payment P
-		LEFT JOIN Points_Group ON P.PaymentID = PG.PaymentID
-		WHERE P.mobileNo = @MobileNo AND P.date_of_payment >= DATEADD(YEAR, -1, CURRENT_TIMESTAMP) AND p.status = 'accepted';
+		LEFT JOIN Points_Group PG ON P.PaymentID = PG.PaymentID
+		WHERE P.mobileNo = @MobileNo AND P.date_of_payment >= DATEADD(YEAR, -1, CURRENT_TIMESTAMP) AND P.status = 'accepted';
 
 		
 	END;
