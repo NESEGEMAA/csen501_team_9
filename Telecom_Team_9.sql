cea65a56cd670c4fa0279a686db708b3e11d9fdc
Ahmed Khalid <ahmedkhalid958666@gmail.com>
2024-11-16 21:38:40 +0200

    changing status in 2.3 f to match the payment status constraints

--- Telecom_Team_9.sql
+++ Telecom_Team_9.sql
@@ -504,7 +504,7 @@ AS
 		SELECT @TotalTransactions = COUNT(P.PaymentID) , @TotalPoints = SUM(ISNULL(PG.pointsAmount,0))
 		FROM Payment P
 		LEFT JOIN Points_Group PG ON P.PaymentID = PG.PaymentID
-		WHERE P.mobileNo = @MobileNo AND P.date_of_payment >= DATEADD(YEAR, -1, CURRENT_TIMESTAMP) AND P.status = 'accepted';
+		WHERE P.mobileNo = @MobileNo AND P.date_of_payment >= DATEADD(YEAR, -1, CURRENT_TIMESTAMP) AND P.status = 'successful';
 	END
 
 	-- Testing
