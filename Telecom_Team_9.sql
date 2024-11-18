9d08a783dfb02dc6d16ae4a84ee485b1202f86c7
NESEGEMAA <nesegemaa@gmail.com>
2024-11-18 22:10:21 +0200

    Updated 2.4 (b)

--- Telecom_Team_9.sql
+++ Telecom_Team_9.sql
@@ -673,10 +673,21 @@ GO;
 CREATE FUNCTION Consumption (@Plan_name ALPHA, @start_date date, @end_date date)
 RETURNS TABLE
 AS
-	RETURN(
-		SELECT U.data_consumption, U.minutes_used, U.SMS_sent
-		FROM Plan_Usage U, Service_Plan P
-		WHERE P.name = @Plan_name AND P.planID = U.planID AND U.start_date = @start_date AND U.end_date = @end_date
+	RETURN (
+		SELECT SUM(O.data_consumption) AS 'Data consumption', SUM(O.minutes_used) AS 'Minutes used', SUM(O.SMS_sent) AS 'SMS sent'
+		FROM (
+			(
+				SELECT U.data_consumption, U.minutes_used, U.SMS_sent
+				FROM Plan_Usage U, Service_Plan P
+				WHERE P.name = @Plan_name AND P.planID = U.planID AND U.start_date >= @start_date
+			)
+			EXCEPT
+			(
+				SELECT U.data_consumption, U.minutes_used, U.SMS_sent
+				FROM Plan_Usage U, Service_Plan P
+				WHERE P.name = @Plan_name AND P.planID = U.planID AND U.end_date <= @end_date
+			)
+		) AS O
 	)
 GO;
 
